8f55e976f3a449d7cacadb743b4c7cf194215262
gloriaghe <gloghe@yahoo.it>
2022-11-21 14:21:42 +0100

    PizzaController con vista index tabella e detail per singola pizza

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,27 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            PizzaDbContext db = new PizzaDbContext();
+
+            List<Pizza> listaPizze = db.Pizzas.ToList();
+            return View(listaPizze);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            PizzaDbContext db = new PizzaDbContext();
+
+            Pizza pizza = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+            return View(pizza);
+        }
+    }
+}
